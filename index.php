853bb16185816797c5d243589cc430e814ae1f9c
brunEdo <edobruno.m@gmail.com>
2021-10-18 16:01:20 +0200

    Print a string and its length

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-<?php 
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
 
+<?php 
+$text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dolorum vel eum cum ratione pariatur molestiae eaque, voluptates corporis. Expedita aliquid reiciendis ullam aliquam dicta nemo deserunt. Placeat, voluptas accusantium.";
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +16,7 @@
     <title>PHP Badwords</title>
 </head>
 <body>
-    
+  <p><?php echo $text ?></p> 
+  <p>The text has <?php echo strlen($text) ?> characters.</p> 
 </body>
 </html>
